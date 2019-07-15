eedb80a9592535acdeb0e1d2c2a3ffe7146bdd57
Priyanka Vagadre <priyanka.vagadre@EXXATSYSTEMS.COM>
2019-07-15 13:16:37 +0530

    counter test actions and reducers

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
 import {  StoreDevtoolsModule} from "@ngrx/store-devtools";
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { counterReducer } from './my-counter/counter.reducers';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoutingComponents,
+    MyCounterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule,
+    StoreModule.forRoot({count : counterReducer}),
     CustomersModule,
     StoreDevtoolsModule.instrument(),
   ],
